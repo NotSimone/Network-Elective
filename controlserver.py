dc51cc93ed2c52c63b2dad080e2230e929492884
Finite-Jest <59790281+Finite-Jest@users.noreply.github.com>
2021-04-20 20:21:57 +1000

    Added message reconstruction and dynamic clients

--- controlserver.py
+++ controlserver.py
@@ -2,100 +2,133 @@
 
 import socket
 import sys
-from queue import Queue
 from select import select
 from typing import List
 import time
+import random
 from server import Server, SnoopedPacket
 
 SNOOP_SERVER_IP = "149.171.36.192"
 SNOOP_SERVER_PORT = 8154
-HTTP_SERVER_PORT = 8080
+HTTP_SERVER_PORT = 8155
 
-snooped_packets: "Queue[SnoopedPacket]" = Queue()
-pkts = {}
+all_packets: List[SnoopedPacket] = []
+invalid_message_length: List[int] = list()
+
+# Send complete message to server
+# Return true if accepted
+def send_message(msg: str) -> bool:
+    ################################
+    #POST
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)    
+    s.connect((SNOOP_SERVER_IP,HTTP_SERVER_PORT))
+    
+    header = "POST /session HTTP/1.1\r\n"
+    host = "Host: " + SNOOP_SERVER_IP + ":8155\r\n"
+    contentLength = "Content-Length: " + str(len(msg)) + "\r\n\r\n"
+    contentType = "Content-Type: text/plain\r\n"
+
+    data = header + host + contentLength + msg
+    s.sendall(str.encode(data))
+    print(s.recv(4096))
+
+    # TODO
+    return True
+
+# Get factors of a number
+# https://stackoverflow.com/questions/6800193/what-is-the-most-efficient-way-of-finding-all-the-factors-of-a-number-in-python
+def factors(n):
+    return set(x for tup in ([i, n//i] 
+                for i in range(1, int(n**0.5)+1) if n % i == 0) for x in tup)
+
+# Attempt to reconstruct the whole message and send it off
+# Return true if successful
+def reconstruct_message() -> bool:
+    # Get list of packet ident where the message had an EOF
+    eof: List[int] = [x.packet_ident for x in all_packets if x.message[-1] == "\x04"]
+
+    if len(eof) < 3:
+        return False
+
+    # Get list of differences in length and determine possible message lengths
+    eof_diff = [eof[x+1] - eof[x] for x in range(len(eof) - 1)]
+    possible_message_len = factors(min(eof_diff))
+
+    # If there is only one possible message length
+    if len(possible_message_len) == 1:
+        message_length = possible_message_len[0]
+    if len(possible_message_len) == 2:
+        message_length = possible_message_len[1]
+
+    # Check each possible message length
+    for x in possible_message_len:
+        message = validate_message_len(x)
+        if (message):
+            send_message(message)
+            return True
+    
+    return False
+    
+# Attempt to construct a message with length
+# Returns empty string on fail
+def validate_message_len(length: int) -> str:
+    # If message length is bad, return early
+    if length in invalid_message_length:
+        return ""
+
+    messages = [None] * length
+    start_ident = all_packets[0].packet_ident
+    # Check validity using all the packets we found so far
+    for x in all_packets:
+        index = (x.packet_ident - start_ident + 1) % length
+        if messages[index] == None:
+            messages[index] = x.message
+        else:
+            # On mismatch, mark length as invalid and return
+            if messages[index] != x.message:
+                invalid_message_length.append(length)
+                return ""
+
+    # Check if we found a whole message
+    missing_messages = [x for x in messages if x == None]
+    if len(missing_messages) == 0:
+        complete_message = "".join(messages)
+        print(complete_message)
+        return complete_message
+
+    return ""
 
 with Server() as server:
     # Connect and configure the clients
     client_count = 1 if len(sys.argv) < 2 else int(sys.argv[1])
+    bitrate = 1000 if len(sys.argv) < 3 else int(sys.argv[2])
+    # Detection timeout
+    timeout = 50 / bitrate
+
     server.connect_clients(client_count)
     server.config_clients(ip=SNOOP_SERVER_IP, port=SNOOP_SERVER_PORT)
     
     ident = 30
     start = time.perf_counter()
     
+    client_responses = client_count
+    
     while True:
-        
-        server.send_snoop_req(0, 1, ident)
-        ident += 1
-        time.sleep(0.1)
+        if client_responses == client_count:
+            if (reconstruct_message()):
+                exit
 
-        if(time.perf_counter()-start > 5): # 5 seconds have elapsed       
-            #print(pkts)
-            ttl_no_unique = len(pkts)
-            unique_pkts = {}
-            eom = -1
-            for key, value in pkts.items():
-                unique_pkts[value%ttl_no_unique] = key
-                if '\x04' in key:
-                    eom = value%ttl_no_unique
-            
-            print(sorted(unique_pkts))
-            print("current pkts no: "+str(ttl_no_unique))
-            # res = set(pkts.values()) # the unique set of messages
-            
-            # #print(res)
-            # pkt_iden = dict((v,k) for k,v in pkts.items())
-            
-            
-            # idenn = -1
-            msg = ''
+            # Add jitter into the timeout to make sure we dont just loop over the same messages
+            jitter = random.randrange(0, 100)
+            time.sleep(timeout + jitter / bitrate)
 
-            # print(len(res))
-            # for x in res:
-            #     idenn = pkt_iden[x]
-            #     if '\x04' in x: 
-            #         eom = idenn%len(res)   
-            #     unique_pkts[idenn%len(res)] = x
-            
-            l = list(unique_pkts.keys())
-            #print(sorted(l)) #id of messages
-            #print(list(range(min(l), max(l)+1)))
+            # Send snoop requests to all clients
+            # Make sure we send requests with consequtive queue positions
+            for n in range(client_count):
+                server.send_snoop_req(n, n+1, ident)
+                ident += 1
 
-            if sorted(l) == list(range(0, max(l)+1)): 
-                #checking if all messages have been received
-                print('snoop completed in (sec): '+str(time.perf_counter()-start))
-                print('total no of packets: '+str(ttl_no_unique))
-                print(sorted(unique_pkts))
-                i = eom+1
-                while i < len(l):
-                    msg += unique_pkts[i]
-                    i+=1
-
-                i = 0
-                while i < eom+1:
-                    msg += unique_pkts[i]
-                    i+=1
-
-                print(msg)
-
-                
-                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)     
-                s.connect(('127.0.0.1',8080))
-                
-                message = "POST /session HTTP/1.1\r\n"
-                Host = "Host: 127.0.0.1:8080\r\n"
-                contentLength = "Content-Length: " + str(len(msg)) + "\r\n\r\n"
-                contentType = "Content-Type: text/plain\r\n"
-        
-                data = message + Host + contentLength + msg
-                s.sendall(str.encode(data))
-                print(s.recv(4096))
-               
-            
-                print('cant send')
-            else:
-                print('missing parts of message, current packets no.: '+str(ttl_no_unique))
+            client_responses = 0
             
 
         # Select on one of the clients returning data
@@ -110,9 +143,8 @@ with Server() as server:
             try:
                 for packet in server.get_snooped_packet(conn):
                     print(f"Got {packet.request_ident};{packet.packet_ident};{packet.message}")
-                    snooped_packets.put(packet)
-                    #pkts[packet.packet_ident] = packet.message
-                    pkts[packet.message] = packet.packet_ident
+                    all_packets.append(packet)
+                    client_responses += 1
             except ConnectionAbortedError:
                 print(f"Client {server.connections.index(conn)} closed connection")
                 exit()
