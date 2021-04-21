f29fcd88ec8bdd9624512cae3f2b9ae8f5ed5f0d
Stephanie Zhen <z5133975@student.unsw.edu.au>
2021-04-21 11:33:21 +1000

    tmp

---
+++ command.sh
@@ -0,0 +1,1 @@
+/usr/local/bin/4123-server -port 8154 -file message.txt -address 149.171.36.192 -verbose
--- controlserver_steph.py
+++ controlserver_steph.py
@@ -5,7 +5,7 @@ import sys
 from select import select
 from typing import List
 import time
-from server import Server, SnoopedPacket
+from server_steph import Server, SnoopedPacket
 
 SNOOP_SERVER_IP = "149.171.36.192"
 SNOOP_SERVER_PORT = 8152
@@ -21,7 +21,7 @@ with Server() as server:
     client_count = 1 if len(sys.argv) < 2 else int(sys.argv[1])
     bitrate = 1000 if len(sys.argv) < 3 else int(sys.argv[2])
     # Snoop delay is 50 characters worth of time
-    timeout = 50 / bitrate
+    #timeout = 50 / bitrate
 
     server.connect_clients(client_count)
     server.config_clients(ip=SNOOP_SERVER_IP, port=SNOOP_SERVER_PORT)
@@ -33,7 +33,7 @@ with Server() as server:
         
         if client_count == 1:
             server.send_snoop_req(0, 1, ident)
-            time.sleep(timeout)
+            time.sleep(0.1)#timeout)
         elif client_count == 2:
             server.send_snoop_req(0, 1, ident)
             server.send_snoop_req(1, 2, ident)
@@ -49,21 +49,23 @@ with Server() as server:
         #Message reconstruction
         if(time.perf_counter()-start > 1): # 5 seconds have elapsed       
             #print(pkts)
-            ttl_no_unique = len(pkts)
+            
             unique_pkts = {}
             eom = 1
             for key, value in pkts.items():
+                print(value,value%ttl_no_unique, ttl_no_unique,key)
                 unique_pkts[value%ttl_no_unique] = key
                 mod.append(value%ttl_no_unique)
                 if '\x04' in key:
                     eom = value%ttl_no_unique
             
             print(sorted(unique_pkts))
-            print("current pkts no: "+str(ttl_no_unique))
 
             msg = ''
             
             l = list(unique_pkts.keys())
+            #for x in l:
+             #   print(unique_pkts[x])
 
             if sorted(l) == list(range(0, max(l)+1)): 
                 #checking if all messages have been received
@@ -98,7 +100,7 @@ with Server() as server:
             
                 print('cant send')
             else:
-                print('missing parts of message, current packets no.: '+str(len(l)))
+                print('missing parts of message, current packets no.: '+str(ttl_no_unique))
             
 
         # Select on one of the clients returning data
@@ -117,6 +119,7 @@ with Server() as server:
                         if packet.packet_ident%ttl_no_unique not in mod:
                             unique_pkts[packet.packet_ident%ttl_no_unique] = packet.message
                     pkts[packet.message] = packet.packet_ident
+                    ttl_no_unique = len(pkts)
             except ConnectionAbortedError:
                 print(f"Client {server.connections.index(conn)} closed connection")
                 exit()
